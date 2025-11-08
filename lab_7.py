3075cc529bdaa442f48b2cdc01a4eb3aabb2a2ed
Felicity H <felicityh15@gmail.com>
2025-11-07 16:22:41 -0800

    Update lab_7.py

--- lab_7.py
+++ lab_7.py
@@ -56,9 +56,9 @@ class StateMachineNode(Node):
         self.tracking_enabled = False
 
         # TODO: Initialize member variables to track detection state
-        self.last_detection_pos = pass # TODO: Store the last detection in the image so that we choose the closest detection in this frame
+        self.last_detection_pos = None # TODO: Store the last detection in the image so that we choose the closest detection in this frame
         self.target_pos = None  # TODO: Store the target's normalized position in the image (range: -0.5 to 0.5, where 0 is center)
-        self.last_detection_time = pass  # TODO: Store the timestamp of the most recent detection for timeout checking
+        self.last_detection_time = None  # TODO: Store the timestamp of the most recent detection for timeout checking
         
         self.get_logger().info('State Machine Node initialized in IDLE state.')
         self.get_logger().info('Use begin_tracking(object) to enable tracking.')
