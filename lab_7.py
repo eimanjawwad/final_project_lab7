1a3d9f7064081caf7bb2908059247094b2e7389b
Felicity H <felicityh15@gmail.com>
2025-11-10 18:26:46 -0800

    adhjs

--- lab_7.py
+++ lab_7.py
@@ -56,9 +56,9 @@ class StateMachineNode(Node):
         self.tracking_enabled = False
 
         # TODO: Initialize member variables to track detection state
-        self.last_detection_pos = None # TODO: Store the last detection in the image so that we choose the closest detection in this frame
-        self.target_pos = None  # TODO: Store the target's normalized position in the image (range: -0.5 to 0.5, where 0 is center)
-        self.last_detection_time = None  # TODO: Store the timestamp of the most recent detection for timeout checking
+        self.last_detection_pos = 0 # TODO: Store the last detection in the image so that we choose the closest detection in this frame
+        self.target_pos = 0  # TODO: Store the target's normalized position in the image (range: -0.5 to 0.5, where 0 is center)
+        self.last_detection_time = self.get_clock().now()  # TODO: Store the timestamp of the most recent detection for timeout checking
         
         self.get_logger().info('State Machine Node initialized in IDLE state.')
         self.get_logger().info('Use begin_tracking(object) to enable tracking.')
